2a140495d05990831b100edfa24d2fb2226f2081
Esther <esthercog97@gmail.com>
2020-10-14 11:14:18 +0100

    Completed Class and State Exercises

--- ruby-fundamentals/class-syntax/exercise1/lib/car.rb
+++ ruby-fundamentals/class-syntax/exercise1/lib/car.rb
@@ -1,2 +1,17 @@
 class Car
+  def initialize
+    @speed = 0
+  end
+
+  def speed
+    @speed
+  end
+
+  def accelerate(number)
+    @speed += number
+  end
+
+  def brake(number)
+    @speed -= number
+  end
 end
--- ruby-fundamentals/class-syntax/exercise2/lib/garage.rb
+++ ruby-fundamentals/class-syntax/exercise2/lib/garage.rb
@@ -6,4 +6,34 @@ class Garage
       {registration_plate: 'SE60 TSW', make: 'honda'}
     ]
   end
+
+  def cars
+    @cars
+  end
+
+  def add(hash)
+    @cars << hash
+  end
+
+  def find(plate)
+    @cars.each do |car|
+      return car if car[:registration_plate] == plate
+    end
+  end
+
+  def remove(plate)
+    @cars.each do |car|
+      if car[:registration_plate] == plate
+        @cars.delete(car)
+      end
+    end
+  end
+
+  def all_of_make(make)
+    search = []
+    @cars.each do |car|
+      search << car if car[:make] == make
+    end
+    search
+  end
 end
